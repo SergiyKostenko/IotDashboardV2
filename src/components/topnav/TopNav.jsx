62e70828e783960da26a281fd63a3e965d1e3510
SergiyKostenko <sergiy.kostenk@gmail.com>
2022-04-27 18:52:40 +0300

    top nav fixes

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -40,10 +40,6 @@ const renderUserMenu = (item, index) => (
 const TopNav = () => {
 	return (
 		<div className='topnav'>
-			<div className='topnav__search'>
-				<input type='text' placeholder='Search here...' />
-				<i className='bx bx-search'></i>
-			</div>
 			<div className='topnav__right'>
 				<div className='topnav__right-item'>
 					{/* dropdown */}
